d2e9609dcc14bb13973604775e0b8d839c563e55
solomonhatendi <solomonc19@gmail.com>
2025-10-19 04:14:38 -0500

    Testing

---
+++ src/main/java/com/phoenixai/transittracker/TransittrackerApplication.java
@@ -0,0 +1,13 @@
+package com.phoenixai.transittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransittrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransittrackerApplication.class, args);
+	}
+
+}
